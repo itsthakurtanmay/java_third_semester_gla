44a1f5156faa4b7f4cbb790c42777640fec5e1b4
itsthakurtanmay <itsthakurtanmay@gmail.com>
2024-08-08 21:04:14 +0530

    Second code

---
+++ Sec BA/firstcode.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * firstcode
+ */
+public class firstcode {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the num: ");
+        int x = sc.nextInt();
+
+        System.out.println(x);
+        sc.close();
+
+        
+
+
+
+    }
+}
